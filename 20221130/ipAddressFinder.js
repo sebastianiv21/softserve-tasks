a691f28c0f3bb8f8cdf5eefe90f52781b5ac31e7
sebastianiv21 <seluti98@gmail.com>
2022-12-03 17:43:58 -0500

    SST-5 ip address finder

---
+++ 20221130/ipAddressFinder.js
@@ -0,0 +1,12 @@
+function ipFinder(str) {
+    const asciiArr = [...str].map(char => char.charCodeAt(0));
+    const sum = asciiArr.reduce((acc, curr) => acc + curr);
+    let address = [];
+    for(let i = 1; i <= 4; i++ ) {
+        address.push(sum * i % 256);
+    }
+    return address
+}
+
+const address = "www.codewars.com" ;
+console.log(ipFinder(address));
