f369d6fa72d43abd980560aee73fd9ccfed27947
sebastianiv21 <seluti98@gmail.com>
2022-12-15 08:01:44 -0500

    SST-17 numeric sequence

--- DEMO1/src/task6.js
+++ DEMO1/src/task6.js
@@ -1,3 +1,18 @@
-const numericSequence = (len, min) => {};
+// Output a comma-separated series of length n consisting of natural numbers whose square is more than the given minimum square.
+
+// Input parameters: length and value of the minimum square
+
+// Output: a string with a series of numbers // len=5, min=4 --> 3,4,5
+
+const numericSequence = (len, min) => {
+    const arr = [];
+    for (let i = 0; arr.length < len; i++) {
+        if (i*i > min) arr.push(i);
+    }
+
+    return arr.join()
+};
+
+console.log(numericSequence(5,4));
 
 export default numericSequence;
--- DEMO1/src/task7.js
+++ DEMO1/src/task7.js
@@ -1,3 +1,9 @@
+// Output all Fibonacci numbers that satisfy the constraint passed to the function: they are in the specified range or have the specified length.
+
+// Input parameters: context object with min and max fields {min: 3, max: 65}, or length field {length: 4}
+
+// Output: array of numbers
+
 const fibonacci = (range) => {};
 
 export default fibonacci;
