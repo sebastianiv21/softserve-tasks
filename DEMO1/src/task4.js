6411b4bd450ba5fc465cdd4b21b5c3ea0109f05f
sebastianiv21 <seluti98@gmail.com>
2022-12-18 19:23:44 -0500

    SST-15 palindrome

--- DEMO1/src/task4.js
+++ DEMO1/src/task4.js
@@ -1,15 +1,37 @@
-const palindrome = (num) => {
-  if (!num)
-  throw { status: 'failed', reason: 'Insert a number input' };
+const isPalindrome = (str) => {
+  const reversedStr = str.split('').reverse().join('');
+  return str == reversedStr;
+};
+
+const extractPalindrome = (num) => {
+  if (!num) throw { status: 'failed', reason: 'Insert a number input' };
   if (typeof num !== 'number')
-    throw { status: 'failed', reason: 'Input\'s type must be number' };
+    throw { status: 'failed', reason: "Input's type must be number" };
   if (num <= 10)
     throw { status: 'failed', reason: 'Input must be greater than ten' };
 
-  const reversedNum = num.toString().split('').reverse().join('');
-  if (num == reversedNum) return num;
-  return 0;
+  let palLength = 0;
+  let palNum = '';
+
+  for (let i = 0; i < num.toString().length; i++) {
+    let subStr = num.toString().substring(i);
+
+    for (let j = subStr.length; j >= 0; j--) {
+      let partOfSubStr = subStr.substring(0, j);
+      if (partOfSubStr.length <= 1) continue;
+
+      if (isPalindrome(partOfSubStr)) {
+        if (partOfSubStr.length > palLength) {
+          palLength = partOfSubStr.length;
+          palNum = Number(partOfSubStr);
+        }
+      }
+    }
+  }
+
+  return palNum == '' ? 0 : palNum;
 };
 
-module.exports = palindrome;
+module.exports = extractPalindrome;
 
+console.log(extractPalindrome(1234437));
